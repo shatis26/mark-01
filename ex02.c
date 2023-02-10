0d227f08f5ae079bd85a4d2f4643736243df5844
shatis26 <125037359+shatis26@users.noreply.github.com>
2023-02-10 19:21:48 +0530

    Add files via upload

---
+++ ex02.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a, b, sum;
+    printf("Enter the first number a:\n");
+    scanf("%d", &a);
+
+    printf("Enter the first number b:\n");
+    scanf("%d", &b);
+
+    sum = a+b;
+
+    printf("The sum is %d \n", sum);
+
+    return 0;
+}
